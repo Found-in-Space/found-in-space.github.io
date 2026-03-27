d5517542426cdaa1063897cacb3c22151831e81b
Kaj Siebert <kaj@k-si.com>
2026-03-27 16:24:32 +0100

    Add build action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+const repository = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+const repositoryOwner = process.env.GITHUB_REPOSITORY_OWNER ?? '';
+const isUserSite = repository.endsWith('.github.io');
+const base =
+	process.env.BASE_PATH ??
+	(isGitHubActions && repository && !isUserSite ? `/${repository}/` : '/');
+const site =
+	process.env.SITE_URL ??
+	(isGitHubActions && repository && repositoryOwner
+		? `https://${repositoryOwner}.github.io${isUserSite ? '/' : base}`
+		: undefined);
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	output: 'static',
+	site,
+	base,
+});
